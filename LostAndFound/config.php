eed03bdbd3ccd657459b37395b4b1e01a4e99a22
Abbas <42001049+iam-abbas@users.noreply.github.com>
2020-05-14 15:19:42 -0700

    Update config.php

--- LostAndFound/config.php
+++ LostAndFound/config.php
@@ -1,5 +1,12 @@
   
 <?php
+error_reporting(E_ALL); // Error engine - always E_ALL!
+ini_set('ignore_repeated_errors', TRUE); // always TRUE
+ini_set('display_errors', FALSE); // Error display - FALSE only in production environment or real server. TRUE in development environment
+ini_set('log_errors', TRUE); // Error logging engine
+ini_set('error_log', 'errors.log'); // Logging file path
+ini_set('log_errors_max_len', 1024); // Logging file size
+
 ob_start();
 @session_save_path("tmp");
 @session_start();
@@ -14,10 +21,5 @@ $con = mysqli_connect($server_name,$mysql_user,$mysql_pass,$db_name);
 date_default_timezone_set('Asia/Kolkata');
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-error_reporting(E_ALL); // Error engine - always E_ALL!
-ini_set('ignore_repeated_errors', TRUE); // always TRUE
-ini_set('display_errors', FALSE); // Error display - FALSE only in production environment or real server. TRUE in development environment
-ini_set('log_errors', TRUE); // Error logging engine
-ini_set('error_log', 'errors.log'); // Logging file path
-ini_set('log_errors_max_len', 1024); // Logging file size
+
 ?>
